277ea105ee3f43047f67e6f4c2a0a2f11f5c3d02
Roman <lomtik1979@gmail.com>
2026-05-08 20:32:12 +0300

    airport database and autocomplete

---
+++ static/airports.js
@@ -0,0 +1,66 @@
+const airports = [
+
+{
+    code: "TLV",
+    city: "Tel Aviv",
+    country: "Israel",
+    flag: "🇮🇱"
+},
+
+{
+    code: "CDG",
+    city: "Paris",
+    country: "France",
+    flag: "🇫🇷"
+},
+
+{
+    code: "ORY",
+    city: "Paris",
+    country: "France",
+    flag: "🇫🇷"
+},
+
+{
+    code: "MAD",
+    city: "Madrid",
+    country: "Spain",
+    flag: "🇪🇸"
+},
+
+{
+    code: "BCN",
+    city: "Barcelona",
+    country: "Spain",
+    flag: "🇪🇸"
+},
+
+{
+    code: "FCO",
+    city: "Rome",
+    country: "Italy",
+    flag: "🇮🇹"
+},
+
+{
+    code: "LHR",
+    city: "London",
+    country: "UK",
+    flag: "🇬🇧"
+},
+
+{
+    code: "ATH",
+    city: "Athens",
+    country: "Greece",
+    flag: "🇬🇷"
+},
+
+{
+    code: "JFK",
+    city: "New York",
+    country: "USA",
+    flag: "🇺🇸"
+}
+
+];
